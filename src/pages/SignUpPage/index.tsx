7fef1c4dafa96d8252f040914d8267454ac31f90
Pedro Frohmut <pedrofrohmut@gmail.com>
2020-10-13 14:43:06 -0300

    SignUp page styles

--- src/pages/SignUpPage/index.tsx
+++ src/pages/SignUpPage/index.tsx
@@ -7,6 +7,33 @@ import { Container } from "./styles"
 const SignUpPage: React.FC = () => (
   <Container className="SignUpPage">
     <PageTitle>Todo App | Sign up</PageTitle>
+    <div className="form-wrapper">
+      <form>
+        <div className="form-group">
+          <label htmlFor="firstName">First Name</label>
+          <input id="firstName" type="text" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="lastName">Last Name</label>
+          <input id="lastName" type="text" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">E-mail</label>
+          <input id="email" type="text" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input id="confirmPassword" type="password" />
+        </div>
+        <div className="form-group">
+          <button type="submit">Sign Up</button>
+        </div>
+      </form>
+    </div>
   </Container>
 )
 
